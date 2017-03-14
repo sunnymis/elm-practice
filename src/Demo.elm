100c576f09b8c99167eca6b18999b5ec941384aa
Mistry, Sunny <sunnyvmistry@gmail.com>
2017-03-13 20:48:29 -0400

    Projects can be added, values reset

--- src/Demo.elm
+++ src/Demo.elm
@@ -30,15 +30,23 @@ type alias Employee =
     }
 
 
+type alias Project =
+    { name : String
+    , founder : String
+    }
+
+
 type alias Model =
     { employee : Employee
+    , project : Project
     , employees : List Employee
+    , projects : List Project
     }
 
 
 model : Model
 model =
-    Model (Employee "" "" FullTime) []
+    Model (Employee "" "" FullTime) (Project "" "") [] [ (Project "WhoZoo" "Simon"), (Project "CodePilot" "Ian"), (Project "PiggyBank" "Tom"), (Project "FreshTracks" "Matt"), (Project "KnowThings" "AK") ]
 
 
 
@@ -50,6 +58,7 @@ type Msg
     | Save
     | FullTimeChecked
     | StudentChecked
+    | SelectProject String
 
 
 update : Msg -> Model -> Model
@@ -65,7 +74,33 @@ update msg model =
             updateRadio msg model
 
         Save ->
-            { model | employees = model.employee :: model.employees }
+            { model | employees = model.employee :: model.employees, employee = Employee "" "" FullTime, project = Project "" "" }
+
+        SelectProject selectedProject ->
+            let
+                newProject =
+                    model.projects
+                        |> List.filter (\p -> p.name == selectedProject)
+                        |> List.head
+                        |> resolveProject
+
+                employee =
+                    model.employee
+
+                newEmployee =
+                    { employee | project = newProject.name }
+            in
+                { model | project = newProject, employee = newEmployee }
+
+
+resolveProject : Maybe Project -> Project
+resolveProject project =
+    case project of
+        Nothing ->
+            Project "" ""
+
+        Just proj ->
+            proj
 
 
 updateName : Model -> String -> Model
@@ -121,22 +156,41 @@ view model =
                 , onInput NameChange
                 ]
                 []
-            , (radio model FullTimeChecked FullTime "Full Time")
-            , (radio model StudentChecked Student "Student")
+            , select [ onInput SelectProject ]
+                (model.projects
+                    |> List.sortBy .name
+                    |> List.map projectView
+                    |> addDefaultSelect model
+                )
+            , (radioView FullTimeChecked "Full Time")
+            , (radioView StudentChecked "Student")
             , button [ type_ "submit" ] [ text "SAVE" ]
             , ul [] (List.map employeeList model.employees)
             ]
         ]
 
 
-radio : Model -> Msg -> EmploymentType -> String -> Html Msg
-radio model msg et str =
+addDefaultSelect : Model -> List (Html Msg) -> List (Html Msg)
+addDefaultSelect model projectList =
+    let
+        default =
+            option [ value "", selected (model.project.name == "") ] [ text "Select Project" ]
+    in
+        default :: projectList
+
+
+projectView : Project -> Html Msg
+projectView project =
+    option [ value project.name ] [ text project.name ]
+
+
+radioView : Msg -> String -> Html Msg
+radioView msg str =
     label []
         [ input
             [ type_ "radio"
             , name "employmentType"
             , onClick msg
-            , checked (model.employee.employmentType == et)
             ]
             []
         , text str
